8030ce2132dc774a96729e830b3d872c922b6ec6
StringEpsilon <stringepsilon@gmail.com>
2021-12-11 01:23:52 +0100

    Chore: Accidentally deleted a method.

--- PhpSerializerNET/Types/PhpDynamicObject.cs
+++ PhpSerializerNET/Types/PhpDynamicObject.cs
@@ -13,14 +13,14 @@ namespace PhpSerializerNET {
 		public PhpDynamicObject() { }
 
 		public void SetClassName(string className)  => _dictionary.SetClassName(className);
-
 		public string GetClassName()  => _dictionary.GetClassName();
+
+		internal void TryAdd(string key, object value) => _dictionary.TryAdd(key, value);
 
 		public override bool TryGetMember(GetMemberBinder binder, out object result)
 		{
 			return this._dictionary.TryGetValue(binder.Name, out result);
 		}
-		
 		public override bool TrySetMember(SetMemberBinder binder, object value)
 		{
 			this._dictionary[binder.Name] = value;
